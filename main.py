02e4f06c836cd7b0fbdd50448f09d83850ebea76
barryyan0121 <yanhan0121@gmail.com>
2021-01-31 03:37:50 -0500

    add pdf reader

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ import glob
 filepath = "resume/*.pdf"
 pdfs = glob.glob(filepath)
 for pdfFile in pdfs:
-<<<<<<< HEAD
     text = ""
     with fitz.open(pdfFile) as doc:
         for page in doc:
             text += page.getText()
     print(text)
-=======
-    doc = fitz.open(pdfFile)
->>>>>>> 467552ae0d818d515ad1136e4a08775dbc3bc3e3
