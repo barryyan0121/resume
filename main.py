ad5d024acf9357f7ed9434dad18e994ba9196411
barryyan0121 <yanhan0121@gmail.com>
2021-02-01 01:51:19 -0500

    develop parsers

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
 import fitz
 import glob
+import os
+import docx2txt
+import textract
+from spacy.pipeline import EntityRuler
+import spacy
+nlp = spacy.load("en_core_web_lg")
+ruler = EntityRuler(nlp)
+patterns = [{"label": "ORG", "pattern": [{"LOWER": "ltd"}, {"LOWER": "llc"}, {"LOWER": "corp"}, {"LOWER": "inc"}]},
+            {"label": "DATE", "pattern": [{"LOWER": "-"}, {"LOWER": " - "}, {"LOWER": "present"}, {"LOWER": "now"}]}]
+ruler.add_patterns(patterns)
+nlp.add_pipe(ruler)
 
-filepath = "resume/*.pdf"
-pdfs = glob.glob(filepath)
-for pdfFile in pdfs:
-    text = ""
-    with fitz.open(pdfFile) as doc:
-        for page in doc:
-            text += page.getText()
-    print(text)
+# read files into text
+
+# filepath = "resume/*"
+# paths = glob.glob(filepath)
+# for filename in paths:
+#     text = ""
+#     ext = os.path.splitext(filename)[-1].lower()
+#     if ext == '.pdf':
+#         with fitz.open(filename) as doc:
+#             for page in doc:
+#                 text += page.getText()
+#     elif ext == '.docx':
+#         text = docx2txt.process(filename)
+#     else:
+#         print(filename, " is not a pdf or docx file!")
+
+    # extract company names
+
+filename = "resume/AASHISH MAHAJAN.pdf"
+#text = textract.process(filename)
+#text = text.decode("utf-8")
+
+text = ""
+with fitz.open(filename) as doc:
+    for page in doc:
+        text += page.getText()
+
+if 'Experience' in text:
+    i = text.index('Experience')
+else:
+    print("Experience not found")
+    if 'EXPERIENCE' in text:
+        i = text.index('EXPERIENCE')
+    else:
+        print("EXPERIENCE not found either, move to next resume")
+text = text[i:]
+doc = nlp(text)
+count = 0
+for i in range(len(doc.ents) - 1):
+    ent = doc.ents[i]
+    ent_next = doc.ents[i + 1]
+    if ent.label_ == "ORG" and ent_next.label_ == "DATE":
+        count += 1
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
+        print(ent_next.text, ent_next.start_char, ent_next.end_char, ent_next.label_)
+print(count)
